7a5c2b032529d4a8192f6b0bc24ec8794d7a58be
mitya <dsladkov@gmail.com>
2024-10-10 10:31:02 +0300

    refactor

--- src/Pet4U.Domain/Shared/Errors.cs
+++ src/Pet4U.Domain/Shared/Errors.cs
@@ -4,16 +4,18 @@ public static class Errors
 {
   public static class General
   {
-    public static Error ValueIsInvalid( string? name = null )
+    public static Error ValueIsInvalid(string? name = null)
     {
       return Error.Validation("value.is.invalid", $"{name ?? "value"} is invalid");
     }
-    public static Error NotFound( Guid? id = null )
+
+    public static Error NotFound(Guid? id = null)
     {
       var forId = id == null ? "": $"for Id '{id}'";
       return Error.NotFound("record.not.found", $"record not found{forId}");
     }
-    public static Error LengthIsInvalid( string? name = null )
+    
+    public static Error LengthIsInvalid(string? name = null)
     {
       var label = name + " " ?? "";
       return Error.Validation("length.is.invalid", $"invalid {label}length ");
