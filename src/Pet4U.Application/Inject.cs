251123b36bc4c1f0a17d9754333624c79c2e4bf4
mitya <dsladkov@gmail.com>
2024-10-10 10:23:55 +0300

    fix

--- src/Pet4U.Application/Inject.cs
+++ src/Pet4U.Application/Inject.cs
@@ -7,8 +7,6 @@ public static class Inject
 {
   public static IServiceCollection AddApplication(this IServiceCollection services)
   {
-
-    services.AddScoped<CreateVolunteerHandler>();
     services.AddScoped<ICreateVolunteerHandler, CreateVolunteerHandler>();  
     return services;
   }
--- src/Pet4U.Domain/Shared/Error.cs
+++ src/Pet4U.Domain/Shared/Error.cs
@@ -6,20 +6,20 @@ public record Error
   public string? Message { get; }
   public ErrorType Type { get; }
 
-  private Error(string code, string message, ErrorType type)
+  private Error( string code, string message, ErrorType type )
   {
     Code = code;
     Message = message;
     Type = type;
   }
 
-  public static Error Validation(string code, string message) => new Error(code, message, ErrorType.Validation);
-  public static Error NotFound(string code, string message) => new Error(code, message, ErrorType.NotFound);
-  public static Error Failure(string code, string message) => new Error(code, message, ErrorType.Failure);
-  public static Error Conflict(string code, string message) => new Error(code, message, ErrorType.Conflict);
-  public static Error None() => new Error("","", ErrorType.None);
+  public static Error Validation( string code, string message ) => new Error( code, message, ErrorType.Validation );
+  public static Error NotFound( string code, string message ) => new Error( code, message, ErrorType.NotFound );
+  public static Error Failure( string code, string message ) => new Error( code, message, ErrorType.Failure );
+  public static Error Conflict( string code, string message ) => new Error( code, message, ErrorType.Conflict );
+  public static Error None() => new Error( "","", ErrorType.None );
 
-  public static implicit operator ErrorType(Error error) => error.Type;
+  public static implicit operator ErrorType( Error error ) => error.Type;
 
 }
 
