b26c9363bb2cd4b58ccffeb8df49f9c7d407ce06
mitya <dsladkov@gmail.com>
2024-10-28 22:25:50 +0300

    SpeciesHandler

--- src/Pet4U.Application/UseCases/CreateSpecies/CreateSpeciesHandler.cs
+++ src/Pet4U.Application/UseCases/CreateSpecies/CreateSpeciesHandler.cs
@@ -1,6 +1,8 @@
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Logging;
 using Pet4U.Domain.Shared;
+using Pet4U.Domain.Shared.Ids;
+using Pet4U.Domain.SpeciesManagement.AgregateRoot;
 
 namespace Pet4U.Application.UseCases.CreateSpecies;
 
@@ -15,6 +17,16 @@ public class CreateSpeciesHandler
 
   public async Task<Result<Guid>> HandleAsync(CreateSpeciesCommand command ,CancellationToken cancellationToken = default)
   {
+    var species = Species.Create
+    (
+      id: SpeciesId.New(),
+      title: command.Title,
+      description: command.Description
+    );
 
+    if(species.IsFailure)
+      return species.Error;
+    
+    return species.Value.Id.Value;
   }
 }
