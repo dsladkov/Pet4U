52d5b42062e2d2f7e510856197ecbe33caf0d660
mitya <dsladkov@gmail.com>
2024-10-29 22:05:15 +0300

    minor fixes

--- src/Pet4U.Infrastructure/Repositories/SpeciesRepository.cs
+++ src/Pet4U.Infrastructure/Repositories/SpeciesRepository.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Pet4U.Application.UseCases.CreateSpecies;
 using Pet4U.Application.UseCases.CreateVolunteer;
 using Pet4U.Domain.PetManagement.AgregateRoot;
 using Pet4U.Domain.Shared;
@@ -7,7 +8,7 @@ using Pet4U.Domain.SpeciesManagement.AgregateRoot;
 
 namespace Pet4U.Infrastructure.Repositories;
 
-public class SpeciesRepository //: IVolunteersRepository
+public class SpeciesRepository : ISpeciesRepository
 {
   private readonly ApplicationDbContext _dbContext;
 
@@ -28,50 +29,38 @@ public class SpeciesRepository //: IVolunteersRepository
     return species;
   }
 
-  public async Task<Result<Volunteer?>>GetByIdAsync(
-    VolunteerId volunteerId, 
+  public async Task<Result<Species?>>GetByIdAsync(
+    SpeciesId speciesId, 
     CancellationToken cancellationToken = default)
   {
-    var volunteer = await _dbContext.Volunteers
-              .Include(v => v.Pets)
-              .FirstOrDefaultAsync(v => v.Id == volunteerId, cancellationToken);
+    var species = await _dbContext.Species
+              .Include(s => s.Breeds)
+              .FirstOrDefaultAsync(s => s.Id == speciesId, cancellationToken);
 
-    if(volunteerId is null)
-      return Errors.General.NotFound(volunteerId!);
+    if(speciesId is null)
+      return Errors.General.NotFound(speciesId!);
 
-    return volunteer;
+    return species;
   }
 
   public async Task<Result<Guid>> Save(
-    Volunteer volunteer, 
+    Species species, 
     CancellationToken cancellationToken = default)
   {
-    //entries for checking the state of Volunteer entity (Changed)
-    //var entries1 = _dbContext.ChangeTracker.Entries<Volunteer>();
 
-    //_dbContext.Update(volunteer);
-
-    //var entries2 = _dbContext.ChangeTracker.Entries<Volunteer>();
-
-    _dbContext.Volunteers.Attach(volunteer); //This line of code for track entity. Ef core should do it by yourself. 
+    _dbContext.Species.Attach(species); //This line of code for track entity. Ef core should do it by yourself. 
 
     await _dbContext.SaveChangesAsync(cancellationToken);
 
-    //var entries3 = _dbContext.ChangeTracker.Entries<Volunteer>();
-
-    return volunteer;
+    return species;
   }
 
-  public async Task<Result<Volunteer?>>Delete(Volunteer volunteer, CancellationToken cancellationToken = default)
+  public async Task<Result<Species?>>Delete(Species species, CancellationToken cancellationToken = default)
   {
-    // var result = _dbContext.ChangeTracker.Entries<Volunteer>()
-    // .Where(v => v.State == EntityState.Detached)
-    // .FirstOrDefault()?.Entity?.Id?? Guid.Empty;
-
-    _dbContext.Volunteers.Remove(volunteer);
+    _dbContext.Species.Remove(species);
 
     await _dbContext.SaveChangesAsync(cancellationToken);
 
-    return volunteer;
+    return species;
   }
 }
